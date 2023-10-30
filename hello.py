4bda6f4c6498084987fe5081112a38009d5b9336
baus5 <65490597+baus5@users.noreply.github.com>
2023-10-31 01:38:11 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-# Hello world :)
+# This program says hello and does other things..
+
+print("Hello, world!")
+print("I am the Program..")
+
+print("What is your name?")
+username = input("> ")
+user_1 = str(username)
+
+print(f"Hi {user_1}! Nice to meet you again?!")
